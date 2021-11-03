2560cdb4a9b9464a51d454056611b3272ea1d55c
Alexey Artamonov <alexey375890@gmail.com>
2021-11-03 21:08:21 +0300

    init project

---
+++ src/main/java/com/gmail/alexey375890/TaskTrackerProjectApplication.java
@@ -0,0 +1,11 @@
+package com.gmail.alexey375890;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gmail/alexey375890/TaskTrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.alexey375890;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
